b7a3f1aef1c7337d9dd0890ebee5530dd81432af
Carlos O'Ryan <coryan@google.com>
2026-05-29 16:27:37 -0400

    feat: support for unknown enum values  (#206)
    Update the generated code and the corresponding tests to support unknown
    enum values.

--- Sources/GoogleCloudSecretmanagerV1/SecretVersion.swift
+++ Sources/GoogleCloudSecretmanagerV1/SecretVersion.swift
@@ -124,32 +124,109 @@ public struct SecretVersion: Codable, Equatable, GoogleCloudWkt._AnyPackable,
   /// it can be accessed.
   ///
   /// [google.cloud.secretmanager.v1.SecretVersion]: <doc:SecretVersion>
-  public enum State: Int, Codable, Equatable, Sendable {
-    case unspecified = 0
-    case enabled = 1
-    case disabled = 2
-    case destroyed = 3
+  public enum State: Codable, Equatable, Sendable {
+    case unspecified
+    case enabled
+    case disabled
+    case destroyed
+    /// Encodes an unknown integer value.
+    ///
+    /// The most common cause for an unknown values is for the service to send
+    /// a value unknown to the library. We recommend you update your library to
+    /// the latest version.
+    case unknownIntValue(Int)
+    /// Encodes an unknown string value.
+    ///
+    /// The most common cause for an unknown values is for the service to send
+    /// a value unknown to the library. We recommend you update your library to
+    /// the latest version.
+    case unknownStringValue(String)
 
     public init() {
       self = .unspecified
     }
 
-    public var stringValue: String {
+    /// Returns the integer value associated with the enumeration.
+    ///
+    /// If the enumeration was initialized with an unknown string value, this returns `nil`.
+    public var intValue: Int? {
+      switch self {
+      case .unspecified: return 0
+      case .enabled: return 1
+      case .disabled: return 2
+      case .destroyed: return 3
+      case .unknownIntValue(let v): return v
+      case .unknownStringValue: return nil
+      }
+    }
+
+    /// Returns the string value (or name) associated with the enumeration.
+    ///
+    /// If the enumeration was initialized with an unknown integer value, this returns `nil`.
+    public var stringValue: String? {
       switch self {
       case .unspecified: return "STATE_UNSPECIFIED"
       case .enabled: return "ENABLED"
       case .disabled: return "DISABLED"
       case .destroyed: return "DESTROYED"
+      case .unknownIntValue: return nil
+      case .unknownStringValue(let v): return v
       }
     }
 
-    public init?(stringValue: String) {
+    /// Initialize from a string value.
+    ///
+    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    public init(stringValue: String) {
       switch stringValue {
       case "STATE_UNSPECIFIED": self = .unspecified
       case "ENABLED": self = .enabled
       case "DISABLED": self = .disabled
       case "DESTROYED": self = .destroyed
-      default: return nil
+      default: self = .unknownStringValue(stringValue)
+      }
+    }
+
+    /// Initialize from an integer value.
+    ///
+    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    public init(intValue: Int) {
+      switch intValue {
+      case 0: self = .unspecified
+      case 1: self = .enabled
+      case 2: self = .disabled
+      case 3: self = .destroyed
+      default: self = .unknownIntValue(intValue)
+      }
+    }
+
+    public init(from decoder: Decoder) throws {
+      let container = try decoder.singleValueContainer()
+      if let v = try? container.decode(Int.self) {
+        self.init(intValue: v)
+        return
+      }
+      if let s = try? container.decode(String.self) {
+        if let v = Int(s) {
+          self.init(intValue: v)
+        } else {
+          self.init(stringValue: s)
+        }
+        return
+      }
+      throw DecodingError.dataCorruptedError(
+        in: container, debugDescription: "Expected enum value, must be integer or string.")
+    }
+
+    public func encode(to encoder: Encoder) throws {
+      var container = encoder.singleValueContainer()
+      switch self {
+      case .unspecified: return try container.encode(0)
+      case .enabled: return try container.encode(1)
+      case .disabled: return try container.encode(2)
+      case .destroyed: return try container.encode(3)
+      case .unknownIntValue(let v): return try container.encode(v)
+      case .unknownStringValue(let v): return try container.encode(v)
       }
     }
   }
