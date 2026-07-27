4360d2ca29c8368497cb28faa120fd6ed3eee4bc
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudSecretmanagerV1/Secret.swift
+++ Sources/GoogleCloudSecretmanagerV1/Secret.swift
@@ -342,7 +342,7 @@ public struct Secret: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:SecretType/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "SECRET_TYPE_UNSPECIFIED": self = .unspecified
@@ -357,7 +357,7 @@ public struct Secret: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:SecretType/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
